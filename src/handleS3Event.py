0d9f5856824217fa676fc34607befef61eb3b35d
George Prichici <georgeprichici@users.noreply.github.com>
2019-06-11 21:39:51 -0700

    Added support for sending the download URL if MetaDefender supports the functionality. For now, it's disabled for both Cloud and Core.

--- src/handleS3Event.py
+++ src/handleS3Event.py
@@ -19,18 +19,25 @@ def handler(event, context):
     
     bucket = obj_details["bucket"]
     obj_key = obj_details["obj_key"]
-    download_path = '/tmp/{}{}'.format(uuid.uuid4(), obj_key)   
+    
     
     metadefender_core_url = os.environ['MetaDefenderCoreURL']
     metadefender_cloud_apikey = os.environ['MetaDefenderCloudAPIkey']
     integration_type = os.environ['MetaDefenderDeployment']
-    analysis_callback_url = os.environ['AnalysisComplateCallback']
-
+    analysis_callback_url = os.environ['AnalysisCompleteCallback']
+    
     s3_client = MetaDefenderS3(bucket)
-    s3_client.download_file(obj_key, download_path)
+    md_api = MetaDefenderCoreAPI(metadefender_core_url) if (integration_type == "MetaDefenderCore") else MetaDefenderCloudAPI(metadefender_cloud_apikey)
+
+    download_path = ""  
+    if md_api.supports_dowload_file:
+        download_path = s3_client.generate_presigned_url(bucket, obj_key)
+    else:
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), obj_key)   
+        s3_client.download_file(obj_key, download_path)
 
     metadefender_filename = "{0}::{1}".format(bucket, obj_key)
-    md_api = MetaDefenderCoreAPI(metadefender_core_url) if (integration_type == "MetaDefenderCore") else MetaDefenderCloudAPI(metadefender_cloud_apikey)
+    
     response = md_api.submit_file(metadefender_filename, download_path, analysis_callback_url)
     
     # s3_client.tag_object(obj_key, new_tags)
